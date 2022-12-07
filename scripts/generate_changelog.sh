2f9a9104adbf2fa3b79215121d41e8f6094c59de
Fabian Jakobs <fabian.jakobs@databricks.com>
2022-12-07 14:11:00 +0100

    add release CI (#8)

---
+++ scripts/generate_changelog.sh
@@ -0,0 +1,23 @@
+set -x
+set -e
+
+git tag -l --sort=-committerdate
+TAG=$(git tag -l --sort=-committerdate | grep -E "release-v(([0-9]+\.){2}[0-9]+)" | head -n1)
+
+latestChangelog=$(mktemp /tmp/generate_changelog.XXXXXX)
+if [[ $TAG ]]; then
+    echo "Release tag found. Generating changelog from $TAG"
+    yarn -s conventional-changelog --tag-prefix="release-v" >> $latestChangelog
+else
+    echo "No release tag matching pattern 'release-v*' found. Generating changelog from begining"
+    yarn -s conventional-changelog >> $latestChangelog
+fi
+
+cat $latestChangelog | grep -Ev "Release: v.+" >> $2
+
+tmpfile=$(mktemp /tmp/generate_changelog.XXXXXX)
+echo "# Release: v$1" >> $tmpfile
+cat $latestChangelog | grep -Ev "Release: v.+" >> $tmpfile
+cat CHANGELOG.md >> $tmpfile
+
+cat $tmpfile > CHANGELOG.md
